e26220bef54c49fb450fb1f742cc4295c6713dc6
sakamoto <pinponsumairu@gmail.com>
2021-06-08 10:36:49 +0900

    update main_test

--- main_test.go
+++ main_test.go
@@ -5,6 +5,6 @@ import "testing"
 func Test_main(t *testing.T) {
 	status := goMain([]string{"oilio", "-h"})
 	if status != 0 {
-		t.Errorf("status code wont 0, but got %d", status)
+		t.Errorf("status code 0, but got %d", status)
 	}
 }
